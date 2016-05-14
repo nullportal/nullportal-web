f1623e1420e021919483dbe71c7787ef5ffd27e6
jm-janzen <jmjanzen@gmail.com>
2016-05-14 12:20:41 -0500

    Read EJS file and send to client, rather than static string
    Still very early stub work, but architecturally beginning
    to resemble goal minimum viable product.

--- nullportal.js
+++ nullportal.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app     = express();
+var fs      = require('fs');
+
+var basePath= process.cwd() + '/nullportal-web/views/';
+var index   = {};
 
 var nullportal = {
 
@@ -7,13 +11,18 @@ var nullportal = {
         app.engine('html', require('ejs').renderFile);
         app.set('view engine', 'html');
         app.use(express.static(__dirname + '/views'));
+
+        fs.readFile(basePath + 'index.ejs', 'utf8', function (error, data) {
+            index = (! error) ? data : '404';
+        });
+
         console.log('[nullportal]:\tSetup Complete');
     },
     hello: function (req, callback) {
 
         /*XXX*/console.log('[nullportal]:\t%j', req.headers);
 
-        callback('<h1>Welcome to Nullportal</h1><p>A bit spartan right now...');
+        callback(index);
 
     }
 };
